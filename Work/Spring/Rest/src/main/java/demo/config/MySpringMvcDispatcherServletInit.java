addd1cab2b0dd48e92ae1cc447eaa508e8d93b55
alungu-godaddy <alungu@godaddy.com>
2022-08-10 17:29:29 +0300

    Simple rest example

---
+++ Work/Spring/Rest/src/main/java/demo/config/MySpringMvcDispatcherServletInit.java
@@ -0,0 +1,20 @@
+package demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { DemoAppConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
